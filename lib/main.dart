9383c5d7869dc6af394cc2ca44b8cefe6efad250
josuastg <pangihutanjosua@gmail.com>
2023-12-11 22:25:54 +0700

    lock oriendtation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hobihub/screens/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); //locked orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(const MyApp());
+  });
 }
-
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xffB55DBE));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
