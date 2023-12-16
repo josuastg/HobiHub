0c0887088bad59ce33233e6abd5797c2f1d8346b
prstwn <admin@prstwn.com>
2023-12-16 09:45:51 +0700

    auto change from flutter in pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hobihub/screens/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); //locked orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(const MyApp());
+  });
 }
-
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xffB55DBE));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: const Scaffold(
         body: SplashScreen(),
