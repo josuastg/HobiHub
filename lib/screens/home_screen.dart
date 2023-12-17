b32709f3eba879e286c54b205f0f46cb8beb4e9c
prstwn <admin@prstwn.com>
2023-12-17 20:32:35 +0700

    able to show Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hobihub/models/hobby.dart';
 import 'package:hobihub/screens/my_profile_screen.dart';
 import 'package:hobihub/widgets/bottom_sheet_menu.dart';
+import 'package:hobihub/widgets/hobby_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,26 +27,79 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        backgroundColor: const Color.fromARGB(255, 181, 93, 190),
+        title: const Text(
+          'Home Screen',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MyProfileScreen()));
-              },
-              child: const Text("Go to My Profile Screen"),
-            ),
+      drawer: const Drawer(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              HobbyCard(hobby: 
+              Hobby(
+                imageUrl: "assets/images/Sports.png",
+                title: "Sports",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              HobbyCard(hobby: 
+              Hobby(
+                imageUrl: "assets/images/Music.png", 
+                title: "Music"
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              HobbyCard(hobby: 
+              Hobby(
+                imageUrl: "assets/images/Travel.png", 
+                title: "Traveling"
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              HobbyCard(hobby: 
+              Hobby(
+                imageUrl: "assets/images/Games.png", 
+                title:"Games"
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // Center(
+              //   child: ElevatedButton(
+              //     onPressed: () {
+              //       Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //               builder: (context) => const MyProfileScreen()));
+              //     },
+              //     child: const Text("Go to My Profile Screen"),
+              //   ),
+              // ),
+              // ElevatedButton(
+              //   onPressed: _openBottomSheetMenu,
+              //   child: const Text("Show Bottom Sheet"),
+              // ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: _openBottomSheetMenu,
-            child: const Text("Show Bottom Sheet"),
-          ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/hobby_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:hobihub/models/hobby.dart';
+import 'package:hobihub/screens/chat_screen.dart';
+
+class HobbyCard extends StatelessWidget {
+  final Hobby hobby;
+
+  const HobbyCard({super.key, required this.hobby});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(12)),
+      child: GestureDetector(
+        onTap: () {
+        _navigateToChatScreen(context, hobby.title);
+      },
+        child: Container(
+          height: 195,
+          width: 330,
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 155, 155, 155),
+                offset: Offset(
+                  0.0, 
+                  0.0
+                ),
+                blurRadius: 0.0,
+                spreadRadius: 0.0
+              ),
+              BoxShadow(
+                color:Colors.white,
+                offset: Offset(
+                  1.0, 
+                  0.5
+                ),
+                blurRadius: 9.0,
+                spreadRadius: 3.0
+              ),
+            ]
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                hobby.imageUrl,
+                width: 330,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                hobby.title,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 181, 93, 190),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _navigateToChatScreen(BuildContext context, String title) {
+  Navigator.push(
+    context, 
+    MaterialPageRoute(
+      builder: (context) => ChatScreen(title: title)));
+}
