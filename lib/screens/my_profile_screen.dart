79dbdd7ef9925b9d7d1c487087c9083dd280b34c
Madaputra31 <madaskate@gmail.com>
2023-12-18 22:54:07 +0700

    able to show profile sceen

--- lib/screens/my_profile_screen.dart
+++ lib/screens/my_profile_screen.dart
@@ -7,10 +7,77 @@ class MyProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Profile Screen'),
-      ),
-      body: const Center(
-        child: Text('This is My Profile Screen'),
+          title: const Text(
+            'Profile',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w700, fontSize: 22),
+          ),
+          backgroundColor: Color.fromARGB(255, 181, 93, 190)),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 180,
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromARGB(255, 181, 93, 190),
+                    fontSize: 16),
+              ),
+              Text(
+                'Email@gmail.com',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromARGB(255, 181, 93, 190),
+                    fontSize: 16),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: 300,
+                height: 40,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color.fromARGB(255, 181, 93, 190),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Edit Profile',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: 300,
+                height: 40,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color.fromARGB(255, 181, 93, 190),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Change Password',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w700)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
