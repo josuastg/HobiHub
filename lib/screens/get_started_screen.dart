5885fe090e2f2266f106bbc92f0e0704a536635c
ketrinvania <you@example.com>
2023-12-23 02:37:59 +0700

    Able to show Get Started Screen

--- lib/screens/get_started_screen.dart
+++ lib/screens/get_started_screen.dart
@@ -1,24 +1,118 @@
 import 'package:flutter/material.dart';
 import 'package:hobihub/screens/login_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class GetStartedScreen extends StatelessWidget {
+class GetStartedScreen extends StatefulWidget {
   const GetStartedScreen({super.key});
 
   @override
+  State<GetStartedScreen> createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Get Started Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginScreen()),
-              );
-            },
-            child: const Text('Go To Login Screen')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: SafeArea(
+          top: false,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SvgPicture.asset("assets/images/Shape.svg"),
+                  const SizedBox(height: 1),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: SvgPicture.asset(
+                        "assets/images/photo.svg",
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      "Get Things Done With HobiHub",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 181, 93, 190),
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "Discover hobby communities that",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 181, 93, 190),
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const Text(
+                      "match, learn, and grow with experts in",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 181, 93, 190),
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const Text(
+                      "their field. Start your hobby journey now!",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 181, 93, 190),
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 69,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 54,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 181, 93, 190),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Get Started",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
