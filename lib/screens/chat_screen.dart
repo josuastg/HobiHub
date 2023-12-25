026775ce267554c6068bc95f7bc8bf9d4a830241
prstwn <admin@prstwn.com>
2023-12-25 18:59:32 +0700

    able to show chatscreen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,17 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:hobihub/models/chat.dart';
+import 'package:hobihub/widgets/bubble_chat.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   final String title;
   const ChatScreen({super.key, required this.title});
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _massageController = TextEditingController();
+
+  bool _isButtonSendDisabled() {
+    return _massageController.text.isEmpty || _massageController.text == null;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List<Chat> chatBubbles = [
+      Chat(
+          text: "Hey everyone! What's up?",
+          name: "Ering Haaland",
+          colorName: "#2A27CE",
+          isSelfChat: false,
+          time: DateTime.now()),
+      Chat(
+          text: "Any exciting sports events happening lately?",
+          name: "Ering Haaland",
+          colorName: "#2A27CE",
+          isSelfChat: false,
+          time: DateTime.now()),
+      Chat(
+          text: "Hey! I just watched an incredible soccer match last night.",
+          name: "Me",
+          colorName: "#FF0000",
+          isSelfChat: true,
+          time: DateTime.now()),
+      Chat(
+          text: "The goals were insane!",
+          name: "Me",
+          colorName: "#FF0000",
+          isSelfChat: true,
+          time: DateTime.now()),
+      Chat(
+          text: "Nice! Which teams were playing?",
+          name: "Kylian Mbappe",
+          colorName: "#CE2781",
+          isSelfChat: false,
+          time: DateTime.now()),
+      Chat(
+          text:"It was Liverpool vs. Manchester City. The energy on the field was unbelievable.",
+          name: "Me",
+          colorName: "#FF0000",
+          isSelfChat: true,
+          time: DateTime.now()),
+      Chat(
+          text: "Oh, I missed that one. Who won?",
+          name: "Karim Benzema",
+          colorName: "#8E27CE",
+          isSelfChat: false,
+          time: DateTime.now()),
+      Chat(
+          text: "its Manchester City have won againts Liverpool!",
+          name: "Me",
+          colorName: "#8E27CE",
+          isSelfChat: true,
+          time: DateTime.now()),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 181, 93, 190),
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             color: Colors.white,
             fontSize: 22,
@@ -20,8 +84,82 @@ class ChatScreen extends StatelessWidget {
         ),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: Center(
-        child: Text('This is Chat Screen for $title'),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: chatBubbles.length,
+                itemBuilder: (context, index) {
+                  bool shouldDisplayName = index == 0 ||
+                      chatBubbles[index].isSelfChat !=
+                          chatBubbles[index - 1].isSelfChat;
+                  return BubbleChat(
+                      chat: chatBubbles[index],
+                      shouldDisplayName: shouldDisplayName);
+                }),
+          ),
+          Container(
+            height: 50,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width - 55,
+                        child: Card(
+                          margin: const EdgeInsets.only(
+                              left: 2, right: 2, bottom: 8),
+                          color: const Color.fromARGB(243, 255, 255, 255),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: TextFormField(
+                            controller: _massageController,
+                            onChanged: (value) {
+                              setState(() {});
+                            },
+                            textAlignVertical: TextAlignVertical.center,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 5,
+                            minLines: 1,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20, bottom: 10),
+                              border: InputBorder.none,
+                              hintText: "Type Your Message...",
+                              hintStyle: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromARGB(117, 124, 124, 124)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8, left: 2),
+                        child: CircleAvatar(
+                          backgroundColor: _isButtonSendDisabled()
+                              ? const Color.fromARGB(243, 230, 230, 230)
+                              : const Color.fromARGB(255, 181, 93, 190),
+                          radius: 25,
+                          child: IconButton(
+                            onPressed: _isButtonSendDisabled() ? null : () {},
+                            icon: const Icon(
+                              Icons.send,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
