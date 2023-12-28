1154d8cca331766d4952917d0a020a6d31693a13
josuastg <pangihutanjosua@gmail.com>
2023-12-28 15:04:57 +0700

    fix conflict dev

--- lib/widgets/bottom_sheet_menu.dart
+++ lib/widgets/bottom_sheet_menu.dart
@@ -25,12 +25,12 @@ class _BottomSheetMenuState extends State<BottomSheetMenu> {
         context,
         MaterialPageRoute(builder: (context) => const HomePage()),
       );
-    } else if (menu.title == '  My Profile') {
+    } else if (menu.title == 'My Profile') {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => const MyProfileScreen()),
       );
-    } else if (menu.title == ' About') {
+    } else if (menu.title == 'About') {
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -58,8 +58,8 @@ class _BottomSheetMenuState extends State<BottomSheetMenu> {
     List<NavigationMenu> menus = [
       NavigationMenu(icon: 'assets/images/icon_home.svg', title: 'Home'),
       NavigationMenu(
-          icon: 'assets/images/icon_profile.svg', title: '  My Profile'),
-      NavigationMenu(icon: 'assets/images/icon_info.svg', title: ' About'),
+          icon: 'assets/images/icon_profile.svg', title: 'My Profile'),
+      NavigationMenu(icon: 'assets/images/icon_info.svg', title: 'About'),
     ];
 
     return SizedBox(
